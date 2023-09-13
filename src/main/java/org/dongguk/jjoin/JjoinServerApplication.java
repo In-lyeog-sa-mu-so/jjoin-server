ad181839812f33e1a8e89f2df056c2d5249fb3dd
HyungJoonSon <kr.dev.hyungjoon@gmail.com>
2023-09-13 15:45:23 +0900

    feat: spring boot was init

---
+++ src/main/java/org/dongguk/jjoin/JjoinServerApplication.java
@@ -0,0 +1,12 @@
+package org.dongguk.jjoin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JjoinServerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(JjoinServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/dongguk/jjoin/JjoinServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dongguk.jjoin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JjoinServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
