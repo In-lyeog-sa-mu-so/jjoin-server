4b92164352ddb37ab94b2329ce04287379070755
prefer52 <tjsgh151@naver.com>
2023-10-11 20:21:33 +0900

    feat: 일정 상세 dto 추가 및 API 구현

--- src/main/java/org/dongguk/jjoin/controller/ClubController.java
+++ src/main/java/org/dongguk/jjoin/controller/ClubController.java
@@ -43,6 +43,12 @@ public class ClubController {
         return scheduleService.readClubSchedules(userId, clubId, page);
     }
 
+    @GetMapping("/{clubId}/schedules/{scheduleId}")
+    public ClubScheduleDetailDto readClubScheduleDetail(@PathVariable Long clubId, @PathVariable Long scheduleId) {
+        Long userId = 1L;
+        return scheduleService.readClubScheduleDetail(userId, clubId, scheduleId);
+    }
+
     @GetMapping("/{clubId}/albums")
     public List<ClubAlbumDto> readClubAlbumList(@PathVariable Long clubId, @RequestParam("page") Long page) {
         return albumService.readClubAlbumList(clubId, page);
